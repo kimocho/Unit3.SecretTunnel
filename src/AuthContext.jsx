8dfe93dcaa8d426979c9c28bc342fce57e924f8e
Samuel Kim <samjkim455@gmail.com>
2025-06-12 22:39:34 -0400

    completed

--- src/AuthContext.jsx
+++ src/AuthContext.jsx
@@ -7,12 +7,27 @@ const AuthContext = createContext();
 export function AuthProvider({ children }) {
   const [token, setToken] = useState();
   const [location, setLocation] = useState("GATE");
+  const [userNameInput, setUserNameInput] = useState('');
 
   // TODO: signup
-
+  const signup = async () => {
+    const response = await fetch(`${API}/signup`, { method: 'POST', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify({ username: userNameInput }) });
+    const responseObj = await response.json();
+    setToken(responseObj.token);
+    setLocation('TABLET');
+  }
   // TODO: authenticate
+  const authenticate = async () => {
+    const response = await fetch(`${API}/authenticate`, { method: 'GET', headers: { 'Content-Type': 'application/json', 'Authorization': `Bearer ${token}` } });
+    const responseJson = await response.json();
+    if (!token) {
+      throw Error('no token');
+    }
+    setLocation('TUNNEL');
+    console.log(responseJson);
+  }
 
-  const value = { location };
+  const value = { location, signup, setLocation, authenticate };
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 }
 
--- src/Entrance.jsx
+++ src/Entrance.jsx
@@ -1,6 +1,14 @@
 /** Users can enter their name to receive a token from the API. */
+import { useAuth } from './AuthContext.jsx';
+
 export default function Entrance() {
   // TODO: call signup when form is submitted
+  const { signup } = useAuth();
+
+  const respond = (FormData) => {
+    const response = FormData.get('name');
+    signup(response);
+  };
 
   return (
     <>
@@ -15,7 +23,7 @@ export default function Entrance() {
         fixed on you. The one on the left opens its mouth, and with a deep,
         rumbling voice, it asks, "Who approaches? Speak your name."
       </p>
-      <form>
+      <form action={respond}>
         <label>
           Name
           <input name="name" />
